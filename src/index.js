c598d409e6b72725ed7f9b5d809e486a024c1631
Nick Gard <nick@webflow.com>
2020-07-24 14:24:10 -0700

    Add better tests, shave off bytes

--- src/index.js
+++ src/index.js
@@ -2,76 +2,90 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.isEqual = (function() {
+  var toString = Object.prototype.toString,
+    keys = Object.keys,
+    getPrototypeOf = Object.getPrototypeOf,
+    getOwnPropertySymbols = Object.getOwnPropertySymbols;
   function checkEquality(a, b, refs) {
-    var var1,
-      var2,
-      var3 = Object.prototype.toString.call(a),
-      var4 = Object.prototype.toString.call(b);
+    var aEntries,
+      bEntries,
+      entry,
+      i,
+      aType = toString.call(a),
+      bType = toString.call(b);
 
+    // trivial case: primitives and referentially equal objects
     if (a === b) return true;
-    if (a === null || b === null) return false;
-    if (refs.indexOf(a) > -1 && refs.indexOf(b) > -1) return true;
-    refs.push(a, b); // save results for self-referential checks
-    if (var3 != var4) return false; // not the same type of objects
 
-    switch (var3) {
-      case "[object Symbol]":
+    // if both are null/undefined, the above check would have returned true
+    if (a == null || b == null) return false;
+
+    // check to see if we've seen this reference before; if yes, return true
+    if (refs.indexOf(a) > -1 && refs.indexOf(b) > -1) return true;
+
+    // save results for circular reference checks
+    refs.push(a, b);
+
+    if (aType != bType) return false; // not the same type of objects
+
+    switch (aType.slice(8, -1)) {
+      case "Symbol":
         return a.valueOf() == b.valueOf();
-      case "[object Date]":
-      case "[object Number]":
+      case "Date":
+      case "Number":
         return +a == +b || (+a != +a && +b != +b); // convert Dates to ms, check for NaN
-      case "[object RegExp]":
-      case "[object Function]":
-      case "[object String]":
-      case "[object Boolean]":
+      case "RegExp":
+      case "Function":
+      case "String":
+      case "Boolean":
         return "" + a == "" + b;
-      case "[object Set]":
-      case "[object Map]": {
-        var1 = a.entries();
-        var2 = b.entries();
-        var3 = false;
-        while (!var3) {
-          var4 = var1.next();
-          var3 = var4.done;
-          if (!checkEquality(var4.value, var2.next().value, refs)) {
+      case "Set":
+      case "Map": {
+        aEntries = a.entries();
+        bEntries = b.entries();
+        do {
+          entry = aEntries.next();
+          if (!checkEquality(entry.value, bEntries.next().value, refs)) {
             return false;
           }
-        }
+        } while (!entry.done);
         return true;
       }
-      case "[object ArrayBuffer]":
+      case "ArrayBuffer":
         (a = new Uint8Array(a)), (b = new Uint8Array(b)); // fall through to be handled as an Array
-      case "[object DataView]":
+      case "DataView":
         (a = new Uint8Array(a.buffer)), (b = new Uint8Array(b.buffer)); // fall through to be handled as an Array
-      case "[object Float32Array]":
-      case "[object Float64Array]":
-      case "[object Int8Array]":
-      case "[object Int16Array]":
-      case "[object Int32Array]":
-      case "[object Uint8Array]":
-      case "[object Uint16Array]":
-      case "[object Uint32Array]":
-      case "[object Uint8ClampedArray]":
-      case "[object Arguments]":
-      case "[object Array]":
+      case "Float32Array":
+      case "Float64Array":
+      case "Int8Array":
+      case "Int16Array":
+      case "Int32Array":
+      case "Uint8Array":
+      case "Uint16Array":
+      case "Uint32Array":
+      case "Uint8ClampedArray":
+      case "Arguments":
+      case "Array":
         if (a.length != b.length) return false;
-        for (var1 = 0; var1 < a.length; var1++) {
-          if (!checkEquality(a[var1], b[var1], refs)) return false;
+        for (i = 0; i < a.length; i++) {
+          if (!(i in a) && !(i in b)) continue; // empty slots are equal
+          // either one slot is empty but not both OR the elements are not equal
+          if (i in a != i in b || !checkEquality(a[i], b[i], refs))
+            return false;
         }
         return true;
-      case "[object Object]":
-        var3 = Object.getOwnPropertySymbols;
-        var1 = var3 ? Object.keys(a).concat(var3(a)) : Object.keys(a);
-        var2 = var3 ? Object.keys(b).concat(var3(b)) : Object.keys(b);
+      case "Object":
+        aEntries = keys(a).concat(
+          getOwnPropertySymbols ? getOwnPropertySymbols(a) : []
+        );
+        bEntries = keys(b).concat(
+          getOwnPropertySymbols ? getOwnPropertySymbols(b) : []
+        );
 
         return (
-          var1.length == var2.length &&
-          checkEquality(
-            Object.getPrototypeOf(a),
-            Object.getPrototypeOf(b),
-            refs
-          ) &&
-          var1.every(function(key) {
+          aEntries.length == bEntries.length &&
+          checkEquality(getPrototypeOf(a), getPrototypeOf(b), refs) &&
+          aEntries.every(function(key) {
             return checkEquality(a[key], b[key], refs);
           })
         );
